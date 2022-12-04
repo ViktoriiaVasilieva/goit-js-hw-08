14dfc684ca4c8370a0e1adf20b6a6db1d7f45848
vesnika <vasiliyeva93@gmail.com>
2022-12-04 19:08:33 +0200

    update form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,21 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
 };
 
-populateTextarea();
-
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 refs.form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
 });
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateTextarea();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
@@ -28,7 +28,10 @@ function onFormSubmit(evt) {
 }
 
 function onTextareaInput(evt) {
-  const message = evt.target.value;
+  const dataForm = {};
+  dataForm.email = refs.email.value;
+  dataForm.textarea = refs.textarea.value;
+  const message = JSON.stringify(dataForm);
   localStorage.setItem(STORAGE_KEY, message);
 }
 
