373d1258d36a5924a43af3f960127d2052a17ff6
vesnika <vasiliyeva93@gmail.com>
2022-12-15 13:43:23 +0200

    update formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,13 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 const STORAGE_KEY = 'feedback-form-state';
-const dataForm = {};
+const formData = {};
 
 populateTextarea();
 
@@ -18,27 +16,30 @@ function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-  const formJSON = JSON.stringify(dataForm);
-  const parsedData = JSON.parse(formJSON);
-  console.log(parsedData);
+
+  savedDataForm(e.currentTarget);
 }
 
 function onFormInput(e) {
-  dataForm.email = refs.email.value;
-  dataForm.message = refs.message.value;
-
-  const message = JSON.stringify(dataForm);
-  localStorage.setItem(STORAGE_KEY, message);
-
-  dataForm[e.target.name] = e.target.value;
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedMessage) {
-    let storageData = JSON.parse(savedMessage);
-    refs.email.value = storageData.email;
-    refs.message.value = storageData.message;
-  }
+  if (!storageData) return;
+  Object.keys(storageData).forEach(key => {
+    formData[key] = storageData[key];
+    const elementsForm = refs.form.querySelector(`[name="${key}"]`);
+    elementsForm.value = storageData[key];
+  });
+}
+
+function savedDataForm(form) {
+  const formJSON = JSON.stringify(formData);
+  const parsedData = JSON.parse(formJSON);
+
+  const enteredData = Object.fromEntries(new FormData(form));
+  console.log('enteredData', parsedData);
 }
